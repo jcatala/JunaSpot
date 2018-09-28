84e2739b180427ffb7194883b6690544800e7344
jcatala <jcatala@alumnos.inf.utfsm.cl>
2018-09-28 15:45:40 -0300

    Agregando proyecto y creación del primer mapa

---
+++ junaspot_proyect/src/pages/home/home.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+//DONE BY ME
+
+import { Geolocation, Geoposition } from "@ionic-native/geolocation";
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  CameraPosition,
+  MarkerOptions,
+  Marker, GoogleMapsAnimation, GoogleMapOptions
+} from "@ionic-native/google-maps";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+
+  map: GoogleMap;
+  user_info: {};
+
+
+  constructor(public navCtrl: NavController,
+              public geolocation: Geolocation,
+              public googleMaps: GoogleMaps) {
+
+  }
+
+  ionViewDidLoad(){
+
+
+
+  };
+
+
+  //metodo a cargo de cargar el mapa
+
+  loadMap(positiona: Geoposition){
+
+    let latitude = positiona.coords.latitude;
+    let longitud = positiona.coords.longitude;
+
+    let element: HTMLElement = document.getElementById("map");
+
+    let map: GoogleMap = this.googleMaps.create(element);
+
+    this.map = map;
+
+    //LAT LNG OBJECT
+    let myPosition: LatLng = new LatLng(latitude, longitud);
+
+    //POSICION DE LA CAMARA
+    let position: CameraPosition<LatLng> = {
+      target: myPosition,
+      zoom:18,
+      tilt:60
+    };
+
+
+    map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      console.log("map rudy");
+
+      this.map.setMyLocationEnabled(true);
+      map.moveCamera(position);
+    })
+
+  }
+
+}
